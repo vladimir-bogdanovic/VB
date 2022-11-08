1fdb0ab268d0e139d202301ff752d7841517cf4c
vladimir-bogdanovic <vladimirbogdanovic44@gmail.com>
2022-11-08 11:05:30 +0100

    eeeeeeeeelp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
